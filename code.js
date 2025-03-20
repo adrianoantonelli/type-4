9ef182dfa7696e9d0fefa5632e65d8b9b75dbac8
adrianoantonelli <adrianoantonelli12@gmail.com>
2025-03-20 19:49:17 +0100

    Initial commit

---
+++ code.js
@@ -0,0 +1,93 @@
+export const configurazione = {
+  testo: "y",
+
+  dimensione: 0.8,
+  interlinea: 0.7,
+  allineamento: "centro",
+  percorsoFont: "./assets/InputMonoCondensed-BoldItalic.ttf",
+
+  sensibilitàMicrofonoBase: 1,
+  densitàPuntiBase: 0.4,
+
+  nascondiInterfaccia: true,
+};
+
+/**
+ * Disegna punto
+ * Metti qui quello che vuoi disegnare per ogni punto della font!
+ *
+ * @typedef {Object} Ingredienti
+ * @property {number} x - La coordinata x del punto
+ * @property {number} y - La coordinata y del punto
+ * @property {number} angolo - L'angolo della curva della font in quel punto
+ * @property {number} indice - Il numero del punto nella sequenza (0, 1, 2, 3, ...)
+ * @property {number} unita - Unita' di misura: corrisponde al 10% della dimensione più piccola della finestra (larghezza o altezza)
+ * @property {number} volume - Il volume del microfono - Varia da 0 a 1
+ * @property {number} frameCount - Il numero di frame passati dall'avvio del programma
+ * @property {number} [alpha] - Device orientation alpha angle (z-axis rotation) - Varia da 0 a 360
+ * @property {number} [beta] - Device orientation beta angle (front-to-back tilt) - Varia da -90 a 90
+ * @property {number} [gamma] - Device orientation gamma angle (left-to-right tilt) - Varia da -90 a 90
+ *
+ * @param {Ingredienti} ingredienti
+ */
+export function disegnaPunto({
+  x,
+  y,
+  angolo,
+  indice,
+  unita,
+  volume,
+  frameCount,
+  alpha = 0,
+  beta = 0,
+  gamma = 0,
+}) {
+  push();
+  translate(x, y);
+  rotate(random(0, 360));
+  translate(volume * 1000, 0);
+  image(img, 0, 0, 20, 20);
+  pop();
+}
+
+let img;
+/**
+ * Carica le risorse necessarie
+ * Esempio: carica immagini, suoni, ecc.
+ */
+export function caricamentoRisorse() {
+  img = loadImage("./assets/fly1fab0.png");
+}
+
+/**
+ * Imposta le impostazioni iniziali
+ * Esempio: impostazioni di frame rate, misura degli angoli, ecc.
+ */
+export function impostazioni() {
+  frameRate(30);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+  imageMode(CENTER);
+}
+
+/**
+ * Disegna sotto i punti
+ * @param {function} disegnaTesto - La funzione che disegna il testo
+ */
+export function sotto(disegnaTesto) {
+  background("#1B1B1B");
+
+  // [INFO] Rimuovi il commento per disegnare il testo
+  fill("#1B1B1B");
+  disegnaTesto();
+}
+
+/**
+ * Disegna sopra i punti
+ * @param {function} disegnaTesto - La funzione che disegna il testo
+ */
+export function sopra(disegnaTesto) {
+  // [INFO] Rimuovi il commento per disegnare il testo
+  // fill("black");
+  // disegnaTesto();
+}
